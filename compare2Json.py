2789a9ea3db8b62cc08c5ac4aa165458d1081280
PondJaᵀᴴ <palapon2545@gmail.com>
2022-11-05 19:50:58 +0700

    *

--- compare2Json.py
+++ compare2Json.py
@@ -4,8 +4,8 @@
 import json
 
 # Change your target json file here
-file1 = "minify_205TextMapTH.json"
-file2 = "minify_206TextMapTH.json"
+file1 = "TextMap/302_TextMapTH.json"
+file2 = "TextMap/301_TextMapTH.json"
 
 data1,data2 = {}, {}
 with open(file1, "r", encoding="utf-8") as f:
@@ -15,21 +15,10 @@ with open(file2, "r", encoding="utf-8") as f:
 
 data3 = {}
 for i in data2:
-    if (i not in data1):
-        data3[i] = {"old": "", "new": data2[i]}
-    elif (data2[i] != data1[i]):
-        data3[i] = {"old": data1[i], "new": data2[i]}
+    if (i in data1) and (data2[i] != data1[i]):
+        data3[i] = {"3.1": data1[i], "3.2": data2[i]}
 
-with open(f"compare_{file1}{file2}.json", "w", encoding="utf-8") as f:
+with open(f"compared.json", "w", encoding="utf-8") as f:
     json.dump(data3, f, sort_keys=True, indent=4, ensure_ascii=False)
     f.close()
     print("Wrote file.")
-
-duplicate = []
-for i in data3:
-    for o in data3:
-        if data3[i]["new"] is data3[o]["old"]:
-            print(f"Duplicate {i} <-> {o}")
-            duplicate.append({i,o})
-            continue
-print(duplicate)
